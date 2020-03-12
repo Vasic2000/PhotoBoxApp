904b77c197abbee319608c9eb379c35c837a1f9b
Vasic2000 <Vasic2000@yandex.ru>
2020-03-12 12:11:16 +0300

    Doesn't work

--- app/src/main/java/cz/vasic2000/myapplication/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/cz/vasic2000/myapplication/ui/gallery/GalleryFragment.kt
@@ -4,28 +4,32 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import cz.vasic2000.myapplication.R
 
 class GalleryFragment : Fragment() {
-
-    private lateinit var galleryViewModel: GalleryViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-                ViewModelProvider(this).get(GalleryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_gallery, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val progress : ProgressBar = view!!.findViewById(R.id.progressBar)
+        val textView : TextView = view!!.findViewById(R.id.text_gallery)
+
+        textView.text = "Try to find images in this phone"
+        progress.visibility = View.VISIBLE
+//
+//        Thread.sleep(1500)
+//
+//        progress.visibility = View.GONE
+        textView.text = "No images"
     }
 }
