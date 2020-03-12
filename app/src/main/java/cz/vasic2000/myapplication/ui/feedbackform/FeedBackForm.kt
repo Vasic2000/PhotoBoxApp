f2266aab7b0428a1649d66b867bbdbafe00ba127
Vasic2000 <Vasic2000@yandex.ru>
2020-03-12 11:12:49 +0300

    Add feedBack fragment

--- app/src/main/java/cz/vasic2000/myapplication/ui/feedbackform/FeedBackForm.kt
+++ app/src/main/java/cz/vasic2000/myapplication/ui/feedbackform/FeedBackForm.kt
@@ -1,4 +1,4 @@
-package cz.vasic2000.myapplication.ui.about
+package cz.vasic2000.myapplication.ui.feedbackform
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,12 +8,12 @@ import android.view.ViewGroup
 
 import cz.vasic2000.myapplication.R
 
-class AboutDeveloper : Fragment() {
+class FeedBackForm : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.about_developer_fragment, container, false)
+        return inflater.inflate(R.layout.fragment_feed_back_form, container, false)
     }
 }
